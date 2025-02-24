24b11a12d92bca58c312ebb466082c8dcd5aaab4
mohammadamini2323 <moh.amini2323@gmail.com>
2025-02-24 12:32:05 -0500

    create a method from git hub gitAndGithubEx.java

--- src/main/java/gitAndGithubEx.java
+++ src/main/java/gitAndGithubEx.java
@@ -2,4 +2,7 @@ public class gitAndGithubEx {
     public static void main(String[] args) {
         //not THING to DO
     }
+    public void remoteMethod(){
+        //it is from remote repo
+    }
 }
